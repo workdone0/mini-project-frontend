435be3ad3ffb2f30645f282cac9a44b8cd2cf815
ayushi25sep <akrishi2509@gmail.com>
2021-01-25 21:19:20 +0530

    mdbootstrap installation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import LandingPage from "./screens/landingPage";
 
+
 class App extends React.Component {
   render() {
     return <LandingPage />;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import reportWebVitals from "./reportWebVitals";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 ReactDOM.render(
   <React.StrictMode>
