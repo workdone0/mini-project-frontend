a78ddfb48f95d0c89f1a9ebcb4ff6c007affa03c
euphelity <46653172+euphelity@users.noreply.github.com>
2021-03-17 12:19:07 +0530

    Corrected contact component

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,40 +1,58 @@
 import React, { Component } from "react";
-import { Row, Col, Input } from "antd";
+import { Row, Col, Input, Divider, Button } from "antd";
 
 import "./styles/contact.css";
 class Contact extends Component {
   render() {
     return (
       <>
-        <Row style={{ textAlign: "center" }}>
-          <Col span={24}>Contact Us</Col>
+        <Row justify="center">
+          <h1 style={{ margin: "20px 0px 0px" }}>Contact Us</h1>
         </Row>
-        <Row>
-          <Col span={12}>
+        <Row justify="center">
+            <div className="contact-divider">
+                <Divider style={{borderTopColor:"black", color:"#ea2c62"}}>get in touch</Divider>
+            </div>
+        </Row>
+        <Row justify="center">
+          <Col className="contact-left" span={12}>
             <h2>Get in Touch</h2>
             <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Dignissimos harum corporis fuga corrupti. Doloribus quis soluta
-              nesciunt veritatis vitae nobis?
+              For every problem that you are facing, you can either contact us through our email-id or you can directly contact us through the given mobile number:
             </p>
+            <div style={{padding:"0rem 2rem"}}>
+                <h3>Email-Id</h3>
+                <p>takeeasy@gmail.com</p>
+                <h3>Contact Number</h3>
+                <p>+91 1234567890</p>
+            </div>
           </Col>
-          <Col span={12}>
-            <h2>Your Query Here</h2>
-            <Row>
-              <Col span={12}>
+          <Col className="contact-right" span={12}>
+            <h2 style={{padding:"0rem 1rem"}}>Your Query Here</h2>
+            <Row className="contact-form">
+              <Col className="contact-input" span={12}>
                 <Input placeholder="name" />
               </Col>
-              <Col span={12}>
+              <Col className="contact-input" span={12}>
                 <Input placeholder="email" />
               </Col>
-            </Row>
-            <Row>
-              <Col span={24}>
+              <Col className="contact-input" span={24}>
                 <Input placeholder="Subject" />
               </Col>
-              <Col span={24}>
+              <Col className="contact-input" span={24}>
                 <Input placeholder="Message" />
               </Col>
+            </Row>
+            <Row justify="center" className="contact-input">
+                <Button
+                    type="primary"
+                    size="large"
+                    style={{
+                    backgroundImage: "linear-gradient(130deg, #f54295, #f58442)",
+                    borderColor: "white",
+                }}>
+                    Send Message
+                </Button>
             </Row>
           </Col>
         </Row>
