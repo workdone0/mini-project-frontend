3639b02ab3f0bca8181fce584d5f2e572e524218
euphelity <46653172+euphelity@users.noreply.github.com>
2021-03-21 15:00:07 +0530

    New links added in the sidenav as collapse

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,85 +1,12 @@
 import React, { Component} from "react";
-import { Row, Col, Collapse, Timeline } from "antd";
-import { ClockCircleOutlined } from '@ant-design/icons';
+import { Row, Col} from "antd";
 import "./styles/dashboard.css";
 
 class Dashboard extends Component {
   render() {
-    const { Panel } = Collapse;
-
-    const text = `A dog is a type of domesticated animal.Known for its loyalty and faithfulness,
-                it can be found as a welcome guest in many households across the world.`;
     return (
       <div>
         <Row>
-            <Col span={24} className="dashboard-container">
-                <Collapse className="site-collapse-custom-collapse" accordion bordered={false}>
-                    <Panel className="site-collapse-custom-panel" header="Conference Room Booking" key="1">
-                    <Timeline className="dashboard-timeline" mode="alternate">
-                        <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
-                        <Timeline.Item color="green">Solve initial network problems 2015-09-01</Timeline.Item>
-                        <Timeline.Item dot={<ClockCircleOutlined style={{ fontSize: '16px' }} />}>
-                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                            laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
-                            beatae vitae dicta sunt explicabo.
-                        </Timeline.Item>
-                        <Timeline.Item color="red">Network problems being solved 2015-09-01</Timeline.Item>
-                        <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
-                        <Timeline.Item dot={<ClockCircleOutlined style={{ fontSize: '16px' }} />}>
-                            Technical testing 2015-09-01
-                        </Timeline.Item>
-                        </Timeline>
-                    </Panel>
-                    <Panel className="site-collapse-custom-panel" header="Hospital Appointment" key="2">
-                    <Timeline className="dashboard-timeline" mode="alternate">
-                        <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
-                        <Timeline.Item color="green">Solve initial network problems 2015-09-01</Timeline.Item>
-                        <Timeline.Item dot={<ClockCircleOutlined style={{ fontSize: '16px' }} />}>
-                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                            laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
-                            beatae vitae dicta sunt explicabo.
-                        </Timeline.Item>
-                        <Timeline.Item color="red">Network problems being solved 2015-09-01</Timeline.Item>
-                        <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
-                        <Timeline.Item dot={<ClockCircleOutlined style={{ fontSize: '16px' }} />}>
-                            Technical testing 2015-09-01
-                        </Timeline.Item>
-                        </Timeline>
-                    </Panel>
-                    <Panel className="site-collapse-custom-panel" header="Event Utility Booking" key="3">
-                    <Timeline className="dashboard-timeline" mode="alternate">
-                        <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
-                        <Timeline.Item color="green">Solve initial network problems 2015-09-01</Timeline.Item>
-                        <Timeline.Item dot={<ClockCircleOutlined style={{ fontSize: '16px' }} />}>
-                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                            laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
-                            beatae vitae dicta sunt explicabo.
-                        </Timeline.Item>
-                        <Timeline.Item color="red">Network problems being solved 2015-09-01</Timeline.Item>
-                        <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
-                        <Timeline.Item dot={<ClockCircleOutlined style={{ fontSize: '16px' }} />}>
-                            Technical testing 2015-09-01
-                        </Timeline.Item>
-                        </Timeline>
-                    </Panel>
-                    <Panel className="site-collapse-custom-panel" header="Hostel Complaints" key="4">
-                    <Timeline className="dashboard-timeline" mode="alternate">
-                        <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
-                        <Timeline.Item color="green">Solve initial network problems 2015-09-01</Timeline.Item>
-                        <Timeline.Item dot={<ClockCircleOutlined style={{ fontSize: '16px' }} />}>
-                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                            laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
-                            beatae vitae dicta sunt explicabo.
-                        </Timeline.Item>
-                        <Timeline.Item color="red">Network problems being solved 2015-09-01</Timeline.Item>
-                        <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
-                        <Timeline.Item dot={<ClockCircleOutlined style={{ fontSize: '16px' }} />}>
-                            Technical testing 2015-09-01
-                        </Timeline.Item>
-                        </Timeline>
-                    </Panel>
-                </Collapse>
-            </Col>
         </Row>
       </div>
     );
--- src/components/sidenav.js
+++ src/components/sidenav.js
@@ -1,18 +1,17 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Row, Col } from "antd";
+import { Row, Col, Collapse} from "antd";
 import ayushi from "../assets/ayushi.jpg";
 import "./styles/sidenav.css";
 import ProfileForm from "./profileForm";
 import Dashboard from "./dashboard";
+    
+const { Panel } = Collapse;
 
 export class Sidenav extends Component {
   render() {
     const isDashboard = this.props.isDashboard;
-    const isProfileForm = this.props.isProfileForm;
-
-    console.log(isDashboard);
-    console.log(isProfileForm);
+    
     return (
       <div>
         <Row className="sidenav-container">
@@ -28,6 +27,16 @@ export class Sidenav extends Component {
             </Row>
             <Row justify="center">
               <a className="sidenav-link">Dashboard</a>
+            </Row>
+            <Row justify="center">
+              <Collapse className="site-collapse-custom-collapse" accordion bordered={false}>
+                <Panel className="site-collapse-custom-panel" header="Services" key="1" style={{borderBottom: "0px", color:"white"}}>
+                  <Row><a className="sidenav-link panel-link">Conference Room Requests</a></Row>
+                  <Row><a className="sidenav-link panel-link">Hospital Appointments</a></Row>
+                  <Row><a className="sidenav-link panel-link">Event Utilities Booked</a></Row>
+                  <Row><a className="sidenav-link panel-link">Hostel Complaints</a></Row>
+                </Panel>
+              </Collapse>
             </Row>
           </Col>
           <Col span={19}>
