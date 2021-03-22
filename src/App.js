16954c1c36da04eceb996e11a9b4c75294d9919b
ayushi25sep <akrishi2509@gmail.com>
2021-03-22 17:57:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -93,6 +93,7 @@ class App extends Component {
               <HospitalForm />
             </Route>
 
+            <Route exact path="/HostelComplaint">
               <HostelComplaint />
             </Route>
 
