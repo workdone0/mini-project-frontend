248301fe819354d6fe27293012cdfcab66723d71
euphelity <46653172+euphelity@users.noreply.github.com>
2021-03-22 11:56:29 +0530

    Noticeboard table completed

--- src/components/noticeBoard.js
+++ src/components/noticeBoard.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Row, Col } from "antd";
 import NoticeBoardCard from "./noticeBoardcard";
 import { getRoomBookingsApi } from "../api/roomBooking";
+import "./styles/noticeBoard.css";
 
 class NoticeBoard extends Component {
   constructor() {
@@ -16,17 +17,43 @@ class NoticeBoard extends Component {
       events: await response.data.data,
     });
   }
+
+  handleColor = (i) =>{
+    if(i%2==0){
+      return "main-container-noticeboard-card color1";
+    }
+    else{
+      return "main-container-noticeboard-card color2";
+    }
+  }
   render() {
     return (
       <div id="home-noticeboard">
         <Row justify="center">
           <h1 style={{ margin: "20px 0px" }}>Notice Board</h1>
         </Row>
-        <Row>
+        <Row justify="center" className="notice-table-heading">
+          <Col span={2}></Col>
+          <Col span={7}>
+            <h2 className="notice-heading">Title</h2>
+          </Col>
+          <Col span={3}></Col>
+          <Col span={4}>
+            <h2 className="notice-heading">
+              Venue
+            </h2>
+          </Col>
+          <Col span={3}></Col>
+          <Col span={4}>
+            <h2 className="notice-heading">Timing</h2>
+          </Col>
+          <Col span={1}></Col>
+        </Row>
+        <Row className="notice-table">
           {this.state.events.map((event, index) => {
             return (
               <Col span={24} key={index}>
-                <NoticeBoardCard event={event} />
+                <NoticeBoardCard event={event} color={this.handleColor(index)}/>
               </Col>
             );
           })}
--- src/components/noticeBoardcard.js
+++ src/components/noticeBoardcard.js
@@ -7,23 +7,23 @@ class NoticeBoardCard extends Component {
     const startTime = new Date(this.props.event.startTime * 60 * 1000);
     const endTime = new Date(this.props.event.endTime * 60 * 1000);
     return (
-      <div id="main-container-noticeboard-card">
+      <div className={this.props.color}>
         <Row justify="center">
-          <Col spna={2}></Col>
-          <Col spna={6}>
+          <Col span={2}></Col>
+          <Col span={7}>
             <h2 className="notice-card-heading">{this.props.event.title}</h2>
           </Col>
-          <Col spna={2}></Col>
-          <Col spna={4}>
+          <Col span={3}></Col>
+          <Col span={4}>
             <h2 className="notice-card-heading">
               {this.props.event.roomId.name}
             </h2>
           </Col>
-          <Col spna={2}></Col>
-          <Col spna={4}>
+          <Col span={3}></Col>
+          <Col span={4}>
             <h2 className="notice-card-heading">{`${startTime.getHours()}:${startTime.getMinutes()} - ${endTime.getHours()}:${endTime.getMinutes()}`}</h2>
           </Col>
-          <Col spna={2}></Col>
+          <Col span={1}></Col>
         </Row>
       </div>
     );
