7ee9779c04ba3f23576c7e4511cd9d215fb31b98
euphelity <46653172+euphelity@users.noreply.github.com>
2021-03-24 17:09:02 +0530

    marquee added in hospital form

--- src/screens/HospitalForm.js
+++ src/screens/HospitalForm.js
@@ -155,7 +155,7 @@ export class HospitalForm extends Component {
                     </Row>
            
 
-            <div className="main-container-about" id="home-about-us" style={{backgroundColor:"RGB(134,254,254,0.1"}}>
+            <div className="main-container-about" id="home-about-us" style={{backgroundColor:"RGB(134,254,254,0.1)"}}>
        
         <Row justify="center">
           <h1 className="about-heading" style={{ margin: "50px 0px",color:"black" }}>
@@ -182,6 +182,15 @@ export class HospitalForm extends Component {
               );
             })}
           </Carousel>
+        </Row>
+        <Row className="hospital-marquee-row">
+          <marquee className="hospital-marquee" direction="left">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-journal-check" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M10.854 6.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 8.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+              <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/>
+              <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/>
+            </svg>This is a sample scrolling text that has scrolls texts to left.
+          </marquee>
         </Row>
         <FooterIcon/>
         <Footer/>
