eb07040f8a2359827aaf1bbc120122af021bf169
Shubham <sk246903@gmail.com>
2021-03-30 22:43:44 +0530

    Modified Card for user dashboard conference booking

--- src/components/conferenceDashboard.js
+++ src/components/conferenceDashboard.js
@@ -38,27 +38,21 @@ class ConferenceDashboard extends Component {
           <h1 style={{ margin: "20px 0px" }}>Conference Requests</h1>
         </Row>
         <Row justify="center" className="conference-table-heading">
-          <Col span={1}></Col>
-          <Col span={6}>
+          <Col className="conference-table-heading-column" span={6}>
             <h2 className="conference-heading">Title</h2>
           </Col>
-          <Col span={1}></Col>
-          <Col span={3}>
+          <Col className="conference-table-heading-column" span={3}>
             <h2 className="conference-heading">Venue</h2>
           </Col>
-          <Col span={1}></Col>
-          <Col span={3}>
+          <Col className="conference-table-heading-column" span={6}>
             <h2 className="conference-heading">Date</h2>
           </Col>
-          <Col span={1}></Col>
-          <Col span={3}>
+          <Col className="conference-table-heading-column" span={6}>
             <h2 className="conference-heading">Time</h2>
           </Col>
-          <Col span={1}></Col>
-          <Col span={3}>
+          <Col className="conference-table-heading-column" span={3}>
             <h2 className="conference-heading">Status</h2>
           </Col>
-          <Col span={1}></Col>
         </Row>
         <Row className="conference-table">
           {toDisplayEvents.map((event, index) => {
--- src/components/conferenceDashboardCard.js
+++ src/components/conferenceDashboardCard.js
@@ -1,39 +1,43 @@
 import React, { Component } from "react";
 import { Row, Col } from "antd";
+import moment from "moment";
+
 import "./styles/conferenceDashboardCard.css";
 
 class ConferenceDashboardCard extends Component {
   render() {
-    const startTime = new Date(this.props.event.startTime * 60 * 1000);
-    const endTime = new Date(this.props.event.endTime * 60 * 1000);
+    const startTime = this.props.event.startTime * 60 * 1000;
+    const endTime = this.props.event.endTime * 60 * 1000;
     return (
       <div className={this.props.color}>
         <Row justify="center">
-          <Col span={1}></Col>
-          <Col span={6}>
+          <Col className="dashboard-card-column" span={6}>
             <h2 className="dashboard-card-heading">{this.props.event.title}</h2>
           </Col>
-          <Col span={1}></Col>
-          <Col span={3}>
+          <Col className="dashboard-card-column" span={3}>
             <h2 className="dashboard-card-heading">
               {this.props.event.roomId.name}
             </h2>
           </Col>
-          <Col span={1}></Col>
-          <Col span={3}>
+          <Col className="dashboard-card-column" span={6}>
             <h2 className="dashboard-card-heading">
-              -
+              {moment(startTime).format("MMMM Do YYYY")}
             </h2>
           </Col>
-          <Col span={1}></Col>
-          <Col span={3}>
-          <h2 className="dashboard-card-heading">{`${startTime.getHours()}:${startTime.getMinutes()} - ${endTime.getHours()}:${endTime.getMinutes()}`}</h2>
+          <Col className="dashboard-card-column" span={6}>
+            <h2 className="dashboard-card-heading">{`${moment(startTime)
+              .format("hh:mm a")
+              .toUpperCase()} to ${moment(endTime)
+              .format("hh:mm a")
+              .toUpperCase()}`}</h2>
           </Col>
-          <Col span={1}></Col>
-          <Col span={3}>
-            -
+          <Col className="dashboard-card-column" span={3}>
+            {this.props.event.status == 0
+              ? "PENDING"
+              : this.props.event.status == 1
+              ? "APPROVED"
+              : "DECLINED"}
           </Col>
-          <Col span={1}></Col>
         </Row>
       </div>
     );
